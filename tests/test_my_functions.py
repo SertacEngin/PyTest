227097fc6dbbaf4d235b43e0690c709220b20dec
SertacEngin <89107036+SertacEngin@users.noreply.github.com>
2025-04-13 22:30:04 +0200

    Add files via upload

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -7,11 +7,16 @@ def test_add():
     assert result == 5
 
 
+def test_add_strings():
+    result = my_functions.add(number_one="I like ", number_two="burgers")
+    assert result == "I like burgers"
+
+
 def test_divide():
     result = my_functions.divide(10, 5)
     assert result == 2
 
 
 def test_divide_by_zero():
-    with pytest.raises(ZeroDivisionError):
+    with pytest.raises(ValueError):
         result = my_functions.divide(10, 0)
